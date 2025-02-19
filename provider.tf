aa99a05e58835266c41b4945184309bf763fbdf6
Bhavani Devulapalli <devulapalli.b@northeastern.edu>
2025-02-18 19:17:08 -0500

    testing the failure of workactions

--- provider.tf
+++ provider.tf
@@ -1,6 +1,10 @@
 
 
 provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
+  region  = var.aws_region profile = var.aws_profile
+}
+
+resource "aws_instance" "example" {
+  ami = "INVALID_AMI_ID" # This is not a valid AMI ID
+  instance_type = "t2.micro"
 }
