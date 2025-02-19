74afd9cc04f551013abc09669647d6e25961d58b
Bhavani Devulapalli <devulapalli.b@northeastern.edu>
2025-02-18 19:25:02 -0500

    corrected code

--- provider.tf
+++ provider.tf
@@ -1,11 +1,7 @@
 
 
 provider "aws" {
-  region  = var.aws_region 
-profile = var.aws_profile
+  region  = var.aws_region
+  profile = var.aws_profile
 }
 
-resource "aws_instance" "example" {
-  ami = "INVALID_AMI_ID" # This is not a valid AMI ID
-  instance_type = "t2.micro"
-}
