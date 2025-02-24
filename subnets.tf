94c1fafcf2e45f96291d639a3ad727af11d763ed
Bhavani Devulapalli <157057909+BhavaniDevulapalli1509@users.noreply.github.com>
2025-02-24 18:42:41 -0500

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "public_subnet" {
 #creating private subnet
 resource "aws_subnet" "private_subnet" {
   count             = length(var.private_subnet_cidrs)
-  vpc_id            = aws_vpc.my_vpc.id
+     vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = var.private_subnet_cidrs[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
